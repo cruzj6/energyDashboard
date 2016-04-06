83ea60ae16e1a5175eb80b77187a73f1dd501642
Joseph Levenson <joseph.levenson@crescendocontent.com>
2016-04-06 17:24:48 -0400

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name energydashApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the energydashApp
+ */
+angular.module('energydashApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name energydashApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the energydashApp
+ */
+angular.module('energydashApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
