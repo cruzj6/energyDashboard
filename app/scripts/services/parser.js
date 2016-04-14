2676ec52b7506fe1edebbf83041dafdf590072dd
Joseph Levenson <joseph.levenson@crescendocontent.com>
2016-04-13 21:08:43 -0400

    Parser done

--- app/scripts/services/parser.js
+++ app/scripts/services/parser.js
@@ -10,4 +10,22 @@
 angular.module('energydashApp')
   .service('Parser', function () {
 
+    return {
+      parse: function (data) {
+        var i,
+            dataLines = data.split("\n"),
+            length = dataLines.length,
+            returnedObj = { dates: [] };
+        returnedObj.name = dataLines[0].split(",")[1];
+        returnedObj.unit = dataLines[1].split(",")[1];
+        for (i = 2; i < length; i += 1) {
+          returnedObj.dates.push({
+            date: dataLines[i].split(",")[0],
+            val: dataLines[i].split(",")[1]
+          });
+        }
+        return returnedObj;
+      }
+    }
+
   });
